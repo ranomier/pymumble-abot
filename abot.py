a59b008c85afb4f3c95322c02913a1842fdf5180
Ranomier <ranomier@fragomat.net>
2016-10-02 00:14:50 +0200

    bugfix: set_bandwith, after start() and is_ready()

--- abot.py
+++ abot.py
@@ -18,7 +18,7 @@ parser.add_argument("-p", "--password", dest="password", type=str, default=None,
 parser.add_argument("-s", "--setperiodsize", dest="periodsize", type=int, default=256,
                     help="Lower values mean less delay. WARNING: Lower values could be unstable")
 
-parser.add_argument("-b", "--bandwidth", dest="bandwidth", type=float, default=96000,
+parser.add_argument("-b", "--bandwidth", dest="bandwidth", type=int, default=96000,
                     help="Bandwith of the bot (in bytes/s). Default value=96000")
 
 args = parser.parse_args()
@@ -29,9 +29,9 @@ else:
     abot = pymumble.Mumble(args.host, args.user)
 abot.set_application_string("abot (%s)" % __version__)
 abot.set_codec_profile("audio")
-abot.set_bandwidth(args.bandwidth)
 abot.start()
 abot.is_ready()
+abot.set_bandwidth(args.bandwidth)
 
 a_in = alsaaudio.PCM(type=alsaaudio.PCM_CAPTURE)
 a_in.setchannels(1)
