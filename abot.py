a6fcd8a4ff601fb4f32e489809812836135b74d9
Ranomier <ranomier@fragomat.net>
2016-10-06 09:17:17 +0200

    wrong ...

--- abot.py
+++ abot.py
@@ -7,11 +7,71 @@ DESC:   a simple bot that receives sound from a sound device (for me jack audio 
 
 import argparse
 import sys
+from multiprocessing import Process
+from time import sleep
 
 from thrd_party import pymumble
 import pyaudio
 __version__ = "0.0.4"
 
+class MuRunner(object):
+    """ TODO """
+    def __init__(self, run_dict, args_dict):
+        self.run_dict = run_dict
+        self.args_dict = args_dict
+        self.__join_dicts()
+        self.run()
+
+    def __join_dicts(self):
+        for name in self.run_dict.keys():
+            if name in self.args_dict:
+                self.run_dict[name]["args"] = self.args_dict[name]
+
+
+    def run(self):
+        """ TODO """
+        for name, cdict in self.run_dict.items():
+            print("generating process for:", name)
+            self.run_dict[name]["process"] = Process(target=cdict["func"], args=cdict["args"])
+            print("starting process for:", name)
+            self.run_dict[name]["process"].start()
+
+    def check(self):
+        """ TODO """
+        print(self.run_dict)
+
+
+
+
+class Audio(object):
+    """ TODO """
+    def __init__(self, mumble_object, args_dict):
+        self.mumble = mumble_object
+        self.run_list = {"input": {"func": self.__input_loop, "process": None},
+                         "output": {"func": self.__output_loop, "process": None}}
+        MuRunner(self.run_list, args_dict)
+
+    def __output_loop(self, periodsize):
+        """ TODO """
+        return None
+
+    def __input_loop(self, periodsize):
+        """ TODO """
+        p_in = pyaudio.PyAudio()
+        stream = p_in.open(input=True,
+                           channels=1,
+                           format=pyaudio.paInt16,
+                           rate=pymumble.constants.PYMUMBLE_SAMPLERATE,
+                           frames_per_buffer=periodsize)
+        while True:
+            data = stream.read(periodsize)
+            if not data:
+                print("no data, exiting loop")
+                break
+            self.mumble.sound_output.add_sound(data)
+            print(data)
+        stream.close()
+        return True
 
 def main():
     """swallows parameter. TODO: move functionality away"""
@@ -37,29 +97,23 @@ def main():
     args = parser.parse_args()
 
     abot = pymumble.Mumble(args.host, args.user, certfile=args.certfile, password=args.password)
-    
+
     abot.set_application_string("abot (%s)" % __version__)
     abot.set_codec_profile("audio")
     abot.start()
     abot.is_ready()
     abot.set_bandwidth(args.bandwidth)
 
-    p_in = pyaudio.PyAudio()
-
-    stream = p_in.open(input=True,
-                       channels=1,
-                       format=pyaudio.paInt16,
-                       rate=pymumble.constants.PYMUMBLE_SAMPLERATE,
-                       frames_per_buffer=args.periodsize)
-
+    Audio(abot, {"output": (args.bandwidth, ), "input": (args.bandwidth, )})
     while True:
-        data = stream.read(args.periodsize)
-        if not data:
-            print("no data, exiting loop")
-            break
-        abot.sound_output.add_sound(data)
-    stream.close()
-    return 0
+        sleep(100)
+
+    #stream = p_in.open(input=True,
+    #                   channels=1,
+    #                   format=pyaudio.paInt16,
+    #                   rate=pymumble.constants.PYMUMBLE_SAMPLERATE,
+    #                   frames_per_buffer=args.periodsize)
+
 
 
 
